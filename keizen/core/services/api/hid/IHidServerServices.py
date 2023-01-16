bf51b018cd9144a625c32fea6e72a2aa35ec972c
ZyroX <79484310+okzyrox@users.noreply.github.com>
2023-01-16 19:55:18 +0000

    Update IHidServerServices.py
    service components misc change

--- keizen/core/services/api/hid/IHidServerServices.py
+++ keizen/core/services/api/hid/IHidServerServices.py
@@ -1,6 +1,6 @@
 from .IHidServer import *
 
-IHidServer_ServiceList = [
+ServiceList = [
     [0x0, '0', 'CreateAppletResource', 'hid::IHidServer'], # 0, 1, 2, 3
     [0x1, '1', 'ActivateDebugPad', 'hid::IHidServer'],
     [],
@@ -8,13 +8,13 @@ IHidServer_ServiceList = [
 ]
 
 def get_servicename_by_hex(hex):
-    for i in IHidServer_ServiceList:
+    for i in ServiceList:
         if str(i[0]) == hex:
             return i[0], i[2] # return hex and name
         elif int(hex, 16) == int(i[1]): # if decimal of hex == int form of hex
             return i[0], i[2], i[3] # return hex and name
 
 def get_servicename_by_cmdint(cmd):
-    for i in IHidServer_ServiceList:
+    for i in ServiceList:
         if i[1] == str(cmd):
             return i[0], i[2], i[3] # return hex, name and service
